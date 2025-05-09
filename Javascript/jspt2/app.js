9882b4967ab86eb5bdf54c5ee2327b482885f1a4
prasad-888 <160460158+prasad-888@users.noreply.github.com>
2025-05-09 19:56:44 +0530

    commit

---
+++ Javascript/jspt2/app.js
@@ -0,0 +1,10 @@
+// console.log("Hello World!");
+// console.log("apna college");
+// let a =10;
+// let b= 5;
+// console.log("sum is",a+b);
+
+
+let pencilPrice = 10;
+let eraserPrice = 5;
+console.log("The total price is :", pencilPrice +  eraserPrice, "Rupees");
