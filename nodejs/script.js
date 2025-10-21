c2b815e01092f16b36e79313dffa1765652166c4
prasad-888 <160460158+prasad-888@users.noreply.github.com>
2025-10-21 19:13:18 +0530

    commit

--- nodejs/script.js
+++ nodejs/script.js
@@ -25,6 +25,30 @@
 // console.log(math.sum(2,2));
 // console.log(math.PI);
 
-const info = require("./Fruits");
+// const info = require("./Fruits");
 
-console.log(info);
+// console.log(info);
+
+
+
+
+
+// require alternative import
+
+import {
+    sum, PI
+}
+from "./math.js"
+
+console.log(sum(1,2));
+console.log(PI);
+
+// we cant selectively load only the pieces we need with require 
+// but with import, we can selectively load only the pieces we need,
+// which can save memory,
+
+// Loading is synchronous for 'require' but can be asynchronous for 'import'
+
+import {generate} from "random-words";
+
+console.log(generate());
